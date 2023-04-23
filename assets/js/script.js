13c2be857099bb482d7f3bd8d69d5e4a0cfc6fa1
Austin W <auswaller@gmail.com>
2023-04-23 11:09:49 -0400

    initial and basic HTML, start of fetch code and using inputted location

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+let formEl = document.getElementById("location-form");
+let locationInputEl = document.getElementById("location-input");
+let locationButtonEl = document.getElementById("location-button");
+
+const apiKey = "8e59e3c1090d9dbee1abe33e67416e56";
+let zipCode = "06248";
+
+locationButtonEl.addEventListener("click", function(event){
+    event.preventDefault();
+    zipCode = locationInputEl.value;
+    console.log(zipCode);
+
+    getLocation();
+});
+
+//getForecast(getLocation());
+
+function getLocation(){
+    fetch("http://api.openweathermap.org/geo/1.0/zip?zip=" + zipCode + "&appid=" + apiKey)
+    .then(function (response){
+        console.log("----- Getting Location -----");
+        console.log(response);
+        if(response.status === 200){
+            return response.json();
+        }
+        throw new Error("Something went wrong");
+    }).then(function (data){
+        console.log(data);
+        getForecast(data);
+    }).catch(function (error){
+        console.log(error);
+    });
+}
+
+function getForecast(forLoc){
+    console.log(forLoc);
+    fetch("http://api.openweathermap.org/data/2.5/forecast?lat=" + forLoc.lat + "&lon=" + forLoc.lon + "&appid=" + apiKey)
+    .then(function (response){
+        console.log("----- Getting Forecast -----");
+        console.log(response);
+        return response.json();
+    }).then(function (data){
+        console.log(data);
+    });
+}
